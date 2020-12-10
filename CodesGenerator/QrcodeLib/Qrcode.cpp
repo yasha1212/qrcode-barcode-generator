9eaa3a91c25297e12584fb6f756b8f0da9be0682
yasha1212 <doenglish1@mail.ru>
2020-12-11 01:09:15 +0300

    Add: logic of dividing a bit sequence into the byte blocks

--- CodesGenerator/QrcodeLib/Qrcode.cpp
+++ CodesGenerator/QrcodeLib/Qrcode.cpp
@@ -1,16 +1,24 @@
 #include "framework.h"
 #include "pch.h"
+#include <math.h>
 #include "QrcodeConstants.h"
 #include "Qrcode.h"
 
 using namespace std;
 
+typedef vector<bool> Bits;
+typedef vector<unsigned char> Bytes;
+typedef vector<vector<unsigned char>> BlocksOfBytes;
+
 int ApplyMask(int x, int y);
 int GetVersion(int bitsAmount);
 bool IsValidInput(string input);
-vector<bool> EncodeString(string input);
-void AddServiceFields(vector<bool>& data, int version, int charAmount);
-void FillSequence(vector<bool>& data, int version);
+Bits EncodeString(string input);
+void AddServiceFields(Bits& data, int version, int charAmount);
+void FillSequence(Bits& data, int version);
+Bytes BitsToBytes(Bits data);
+BlocksOfBytes GetDataBlocks(Bits data, int version);
+BlocksOfBytes GetCorrectionBlocks(BlocksOfBytes blocks, int version);
 
 namespace Qrcode 
 {
@@ -21,7 +29,7 @@ namespace Qrcode
             return 2;
         }
 
-        vector<bool> data = EncodeString(input);
+        Bits data = EncodeString(input);
         int version = GetVersion(data.size());
 
         if (version == 0) 
@@ -32,6 +40,8 @@ namespace Qrcode
         AddServiceFields(data, version, input.length());
         FillSequence(data, version);
 
+        BlocksOfBytes dataBlocks = GetDataBlocks(data, version);
+        BlocksOfBytes correctionBlocks = GetCorrectionBlocks(dataBlocks, version);
         return 0;
     }
 }
@@ -57,9 +67,9 @@ int ApplyMask(int x, int y)
     return (x / 3 + y / 2) % 2;
 }
 
-vector<bool> EncodeString(string input) 
+Bits EncodeString(string input) 
 {
-    vector<bool> result;
+    Bits result;
 
     int sum = 0;
 
@@ -115,9 +125,9 @@ int GetVersion(int bitsAmount)
     return version;
 }
 
-void AddServiceFields(vector<bool>& data, int version, int charAmount) 
+void AddServiceFields(Bits& data, int version, int charAmount) 
 {
-    vector<bool> result;
+    Bits result;
 
     for (auto bit : CODING_TYPE_FIELD) 
     {
@@ -143,10 +153,10 @@ void AddServiceFields(vector<bool>& data, int version, int charAmount)
     }
 }
 
-void FillSequence(vector<bool>& data, int version) 
+void FillSequence(Bits& data, int version) 
 {
-    const vector<bool> PADDING_BYTE_1 {1,1,1,0,1,1,0,0};
-    const vector<bool> PADDING_BYTE_2 {0,0,0,1,0,0,0,1};
+    const Bits PADDING_BYTE_1 {1,1,1,0,1,1,0,0};
+    const Bits PADDING_BYTE_2 {0,0,0,1,0,0,0,1};
 
     int paddingBitsAmount = 8 - (data.size() % 8);
 
@@ -159,7 +169,7 @@ void FillSequence(vector<bool>& data, int version)
 
     while (data.size() != VERSION_PARAMETERS.at(version)[MAX_BITS]) 
     {
-        vector<bool> paddingByte = iterator == false ? PADDING_BYTE_1 : PADDING_BYTE_2;
+        Bits paddingByte = iterator == false ? PADDING_BYTE_1 : PADDING_BYTE_2;
 
         for (auto bit : paddingByte) 
         {
@@ -168,4 +178,58 @@ void FillSequence(vector<bool>& data, int version)
 
         iterator = !iterator;
     }
+}
+
+Bytes BitsToBytes(Bits data)
+{
+    Bytes result;
+
+    for (int i = 0; i < data.size(); i += 8) 
+    {
+        unsigned char byte = 0;
+
+        for (int j = 0; j < 8; j++) 
+        {
+            byte += data[i + j] * (pow(2, 7 - j));
+        }
+
+        result.push_back(byte);
+    }
+
+    return result;
+}
+
+BlocksOfBytes GetDataBlocks(Bits data, int version) 
+{
+    Bytes dataBytes = BitsToBytes(data);
+
+    int blocksAmount = VERSION_PARAMETERS.at(version)[BLOCKS_AMOUNT];
+
+    BlocksOfBytes result;
+
+    int bytesInBlock = dataBytes.size() / blocksAmount;
+    int extendedBlocksCount = dataBytes.size() % blocksAmount;
+    int dataIndex = 0;
+
+    for (int i = 0; i < blocksAmount; i++) 
+    {
+        Bytes bytes;
+
+        int extraByte = i < blocksAmount - extendedBlocksCount ? 0 : 1;
+
+        for (int j = 0; j < bytesInBlock + extraByte; j++) 
+        {
+            bytes.push_back(dataBytes[dataIndex]);
+            dataIndex++;
+        }
+
+        result.push_back(bytes);
+    }
+
+    return result;
+}
+
+BlocksOfBytes GetCorrectionBlocks(BlocksOfBytes blocks, int version)
+{
+
 }
